d488c4894871c5f893439a23fecd272e0311497d
Herinihaja rasendrahasinarivo <herinihaja.rasendrahasinarivo@etu.unice.fr>
2020-08-05 13:14:25 +0400

    push projet

---
+++ src/main/java/com/itu/companion/CompanionApplication.java
@@ -0,0 +1,13 @@
+package com.itu.companion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itu/companion/CompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itu.companion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
